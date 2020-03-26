1d1b9e77f3886b2586a554fda6bf0c6ee5c2afd2
piotrstefanski <piotr.stefanski@sts.pl>
2020-03-26 17:44:35 +0100

    detect child nodes after create parent node ex rootNode

--- Node.py
+++ Node.py
@@ -1,7 +1,20 @@
+import main
+import functions
+
 class Node:
+    children = None
+
     def __init__(self, attributes, decisions):
         self.decisions = decisions
         self.attributes = attributes
 
+        self.findChildren()
+
     def __str__(self):
-        return "attributes: {} ; decisiions: {}".format(self.attributes, self.decisions)
+        return "attributes: {} ; decisions: {}".format(self.attributes, self.decisions)
+
+    def findChildren(self):
+        gainRatios = main.getGainRatiosForNode(self)
+        if main.isContinueToBuildingTree(gainRatios):
+            nodes = functions.findNodeChildren(gainRatios, self)
+            self.children = nodes
