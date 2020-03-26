dd08eef06de8a086c58270bd0038769427bb3b5b
piotrstefanski <piotr.stefanski@sts.pl>
2020-03-26 18:59:53 +0100

    add to Node few fields for visualization tree

--- Node.py
+++ Node.py
@@ -3,18 +3,22 @@ import functions
 
 class Node:
     children = None
+    finalDecision = None
+    attributeIndex = None
 
-    def __init__(self, attributes, decisions):
+    def __init__(self, attributes, decisions, question=None):
+        self.question = question
         self.decisions = decisions
         self.attributes = attributes
 
         self.findChildren()
 
     def __str__(self):
-        return "attributes: {} ; decisions: {}".format(self.attributes, self.decisions)
+        return "attributes: {}; decisions: {}; attribute index: {}; question: {}".format(self.attributes, self.decisions, self.attributeIndex, self.question)
 
     def findChildren(self):
         gainRatios = main.getGainRatiosForNode(self)
         if main.isContinueToBuildingTree(gainRatios):
-            nodes = functions.findNodeChildren(gainRatios, self)
-            self.children = nodes
+            self.children = functions.findNodeChildren(gainRatios, self)
+        else:
+            self.finalDecision = self.decisions[0]
