3e3da45769661b55e8433ed1d92383bb5c1910e4
piotrstefanski <piotr.stefanski@sts.pl>
2020-03-26 17:45:24 +0100

    delete useless code

--- main.py
+++ main.py
@@ -3,23 +3,11 @@ import calculate_entropy
 import calculate_information
 import calculate_gain_ratio
 
-def getGainRatioForData(attributes, decisions):
-    # 2 liczba możliwych wartości atrybutów
-    uniqueValues = functions.getCountOccurValuesFromColumns(attributes)
-    countUniqueValues = [len(uniqueValues[i]) for i in uniqueValues]
+def getGainRatiosForNode(Node):
+    countOccurValuesFromColumns = functions.getCountOccurValuesFromColumns(Node.attributes)
 
-    # 3 wyliczyć wystąpienia poszczególnych atrybutów w kolumnach
-    countOccurValuesFromColumns = functions.getCountOccurValuesFromColumns(attributes)
-
-    # wyliczanie entropii
-    occursOfDecision = functions.getCountOccurValuesFromColumns(decisions)[0]
-    entropy = calculate_entropy.calculate(
-        calculate_entropy.calculatePs(occursOfDecision)
-    )
-
-    # wyliczanie zrównoważonego przyrostu informacji
-    information = calculate_information.calculate(decisions, attributes, countOccurValuesFromColumns)
-    gainRatio = calculate_gain_ratio.calculate(attributes, countOccurValuesFromColumns, information)
+    information = calculate_information.calculate(Node.decisions, Node.attributes, countOccurValuesFromColumns)
+    gainRatio = calculate_gain_ratio.calculate(Node.attributes, countOccurValuesFromColumns, information)
 
     return gainRatio
 
