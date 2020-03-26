2138f5fef5989e27852a5df878e1a868508f2086
piotrstefanski <piotr.stefanski@sts.pl>
2020-03-26 17:47:40 +0100

    in index script create only root node

--- index.py
+++ index.py
@@ -1,11 +1,9 @@
 import functions
 import main
+import Node
 
 data = functions.read_file('data/test3.txt')
 rows = data.split('\n')
 
 attributes, decisions = functions.getDecisionsAndAttributesFromRows(rows)
-
-gainRatio = main.getGainRatioForData(attributes, decisions)
-splitAttributeIndex = gainRatio.index(max(gainRatio))
-functions.getNewNodesByAttribute(splitAttributeIndex, attributes, decisions)
+rootNode = Node.Node(attributes, decisions)
